3ac622a6ea96f73d7bf622bcfadb0b3cf230368d
TsvetkovArtem <artem.tsvetkov.95@bk.ru>
2018-04-06 00:35:58 +0300

    add interfaces

---
+++ ServerKeyLogsParser/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerKeyLogsParser
+{
+    interface Command
+    {
+        void setModelState();
+        void execute();
+    }
+}
+/*
+ * Представляет собой объект команды, выполняет некоторые действия и хранит стейт модели на случай отката
+ */
---
+++ ServerKeyLogsParser/LogHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerKeyLogsParser
+{
+    interface LogHandler
+    {
+        void setNextHandler();
+        void parse();//Тестирует возможность парсинга текущего лог-файла и парсит, 
+                     //если возможно, иначе отдаст управление другому хэндлеру
+    }
+}
+/*
+Представляет собой часть паттерна "Цепочка обязанностей", релизует парсинг конкретного лог-файла, если не
+может его распарсить, то передает выполнение другому парсеру 
+*/
